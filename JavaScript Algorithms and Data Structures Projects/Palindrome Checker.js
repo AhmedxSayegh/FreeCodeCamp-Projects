d2a85c4bfdcbad8019cb82e5d065ea2d95ff9cb6
Ahmed Sayegh <86713746+AhmedxSayegh@users.noreply.github.com>
2026-02-14 20:27:12 +0300

    Update Palindrome Checker.js

--- JavaScript Algorithms and Data Structures Projects/Palindrome Checker.js
+++ JavaScript Algorithms and Data Structures Projects/Palindrome Checker.js
@@ -1,5 +1,9 @@
 function palindrome(str) {
-  return true;
+  str = str.toLowerCase()
+  str = str.replace(/[^a-z0-9]+/ig, '')
+  console.log(str)
+  let newStr = [...Array(str.length)].map((_, index) => str[str.length - index - 1]).join('')
+  return str == newStr
 }
 
-palindrome("eye");
+palindrome("A man, a plan, a canal. Panama");
